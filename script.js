28ab347e8c9c8d53ab88a1e330bc0a40f147f390
AdamSkuse <a.t.skuse@gmail.com>
2017-02-18 01:13:20 +0000

    Completes '- it should declare the winner if a player gets three of their symbols in a row '

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var cells = document.getElementsByClassName("cell");
 var playerXToMove = true;
+var movesLog = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+var totalsArray = [];
 
 
 for (var i = 0; i < cells.length; i++) {
@@ -17,8 +19,9 @@ function playerMove(){
             var icon = iconO();
         }
         this.appendChild(icon);
+        checkForWinner(this.id, playerXToMove);
         playerXToMove = !playerXToMove;
-    }
+            }
 }
 
 function iconX() {
@@ -35,4 +38,34 @@ function iconO() {
     icon.classList.add("fa-genderless");
     icon.style="font-size:72px;color:green";
     return icon;
+}
+
+function checkForWinner(index, playerX) {
+    console.log(index, playerX);
+    var playerNumber = 0;
+    if (playerX === true) {
+        playerNumber = 1;
+    } else {
+        playerNumber = 5;
+    }
+    console.log(playerNumber);
+    movesLog[index] = playerNumber;
+    console.log(movesLog);
+    totalsArray = [];
+    totalsArray.push(movesLog[0] + movesLog[1] + movesLog[2]);
+    totalsArray.push(movesLog[3] + movesLog[4] + movesLog[5]);
+    totalsArray.push(movesLog[6] + movesLog[7] + movesLog[8]);
+    totalsArray.push(movesLog[0] + movesLog[3] + movesLog[6]); 
+    totalsArray.push(movesLog[1] + movesLog[4] + movesLog[7]);
+    totalsArray.push(movesLog[2] + movesLog[5] + movesLog[8]);
+    totalsArray.push(movesLog[0] + movesLog[4] + movesLog[8]);
+    totalsArray.push(movesLog[2] + movesLog[4] + movesLog[6]);
+    console.log(totalsArray);
+   
+    if (totalsArray.includes(3)) {
+        alert("X wins!");
+    } else if (totalsArray.includes(15)) {
+        alert("O wins!");
+    }
+
 }
